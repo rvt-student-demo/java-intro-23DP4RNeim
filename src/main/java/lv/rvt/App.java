641a3c4a150e077cecc918e867c8ffdbd1376732
23DP4RNeim <166484439+23DP4RNeim@users.noreply.github.com>
2024-09-20 10:38:34 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,30 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
-    System.out.println("Hello world");
    
+
+        Scanner scanner = new Scanner(System.in);
+
+
+System.out.println("give me the first number:");
+
+int pirmais = Integer.valueOf(scanner.nextLine());
+
+
+System.out.println("give me the second number");
+
+int otrais = Integer.valueOf(scanner.nextLine());
+
+System.out.println(pirmais + otrais);
+System.out.println(pirmais - otrais);
+System.out.println(pirmais * otrais);
+double result = pirmais / otrais * 1.0;
+
+System.out.println(result);
     }
 }
