8ed2e52b5db8c8a826dc8069b6371e92c4ab5d2d
23DP4RNeim <166484439+23DP4RNeim@users.noreply.github.com>
2024-10-10 12:06:48 +0000

    Factorial

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,25 +6,21 @@ public class App
 {
     public static void main( String[] args )
     {
-   
-
-        Scanner scanner = new Scanner(System.in);
 
 
-System.out.println("give me the first number:");
+                Scanner scanner = new Scanner(System.in);
+                
+                System.out.print("Ievadiet skaitli zem 100: ");
+                int userInput = scanner.nextInt();
+    
+                for (int i = userInput; i <= 100; i++) {
+                    System.out.println(i);
+                }
+                
+                scanner.close();
+            
+        }
+        
 
-int pirmais = Integer.valueOf(scanner.nextLine());
-
-
-System.out.println("give me the second number");
-
-int otrais = Integer.valueOf(scanner.nextLine());
-
-System.out.println(pirmais + otrais);
-System.out.println(pirmais - otrais);
-System.out.println(pirmais * otrais);
-double result = pirmais / otrais * 1.0;
-
-System.out.println(result);
-    }
 }
+
