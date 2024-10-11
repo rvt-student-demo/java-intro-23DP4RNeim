f9d565cf5ce6f7b4b2595f7256db53f041bb51ee
23DP4RNeim <166484439+23DP4RNeim@users.noreply.github.com>
2024-10-11 09:55:43 +0000

    Even and odd numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,21 +6,43 @@ public class App
 {
     public static void main( String[] args )
     {
-
-
                 Scanner scanner = new Scanner(System.in);
                 
-                System.out.print("Ievadiet skaitli zem 100: ");
-                int userInput = scanner.nextInt();
-    
-                for (int i = userInput; i <= 100; i++) {
-                    System.out.println(i);
-                }
+                int total = 0;
+                int count = 0;
+                int evenCount = 0;
+                int oddCount = 0;
+        
+                System.out.println("Give numbers:");
                 
+                while (true) {
+                    int number = scanner.nextInt();
+                    if (number == -1) {
+                        break;
+                    }
+                    total += number;
+                    count++;
+        
+                    if (number % 2 == 0) {
+                        evenCount++;
+                    } else {
+                        oddCount++;
+                    }
+                }
+        
                 scanner.close();
-            
+        
+                double average = (count > 0) ? (double) total / count : 0;
+        
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + total);
+                System.out.println("Numbers: " + count);
+                System.out.println("Average: " + average);
+                System.out.println("Even: " + evenCount);
+                System.out.println("Odd: " + oddCount);
+            }
         }
         
-
-}
-
+        
+            
+    
