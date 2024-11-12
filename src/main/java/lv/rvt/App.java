e5005b5517ed4a4a9571447e0b31c055088d95e9
23DP4RNeim <166484439+23DP4RNeim@users.noreply.github.com>
2024-11-12 13:32:49 +0000

    DecreasingCounter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,28 +1,37 @@
 package lv.rvt;
 
-import java.util.*;
+// import java.util.*;
 
-public class App 
-{
-    public class DivisibleByThree {
+public class App {
 
-        public static void divisibleByThreeInRange(int beginning, int end) {
-            for (int i = beginning; i <= end; i++) {
-                if (i % 3 == 0) {
-                    System.out.println(i);
-                }
-            }
-        }
     
-        public static void main(String[] args) {
-            // Example usage
-            divisibleByThreeInRange(3, 6);
-            System.out.println();
-            divisibleByThreeInRange(2, 10);
-        }
+
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+
+        counter = new DecreasingCounter(2);
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+
+        counter = new DecreasingCounter(100);
+        counter.printValue();
+        counter.reset();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
     }
-    
-    }
+}
     
         
     
---
+++ src/main/java/lv/rvt/DecreasingCounter.java
@@ -0,0 +1,27 @@
+package lv.rvt;
+
+public class DecreasingCounter {
+   private int value;
+
+    public DecreasingCounter(int initialValue) {
+        this.value = initialValue;
+
+    }
+
+
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrement() {
+        if (this.value > 0){
+            this.value -= 1;
+        }
+    }
+
+
+    public void reset() {
+        this.value = 0;
+
+    }
+}
